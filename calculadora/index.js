9fbb61b861dcad1f124ecb8fca32bddbd0f3a9ce
Andrezz7 <andrecfonseca2007@gmail.com>
2025-06-17 18:20:54 -0300

    17/06

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,23 +1,22 @@
 function calcular(operador) {
-    const num1 = (document.getElementById('num1').value);
-    const num2 = (document.getElementById('num2').value);
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
     const resultadoInput = document.getElementById('resultado');
 
     // Verifica se os valores são válidos
     if (isNaN(num1) || isNaN(num2)) {
-    resultadoInput.value = "Preencha os dois números!";
-    return;
+        resultadoInput.value = "Preencha os dois números!";
+        return;
     }
 
     // Fazer o cálculo
     let resultado;
     if (operador === '+') {
-    resultado = num1 + num2;
+        resultado = num1 + num2;
     } else if (operador === '-') {
-    resultado = num1 - num2;
+        resultado = num1 - num2;
     }
 
     // Exibe o resultado no input final
     resultadoInput.value = resultado;
-    }
-    
+}
